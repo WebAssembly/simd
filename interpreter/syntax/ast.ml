9ace13b934cb627404de70b4888dbcfa28303018
Ng Zhi An <zhin@google.com>
2020-08-03 09:30:37 -0700

    Better name for extract type

--- interpreter/syntax/ast.ml
+++ interpreter/syntax/ast.ml
@@ -74,10 +74,10 @@ struct
   type relop = TodoRelOp
   type vcvtop = Splat
   type cvtop = (vcvtop, vcvtop, vcvtop, vcvtop, vcvtop, vcvtop, vcvtop) v128op
-  type e =
+  type extract =
       extension (* used for extracting I8 and I16 *)
     * int       (* lane index *)
-  type extractop = (e, e, e, e, e, e, e) v128op
+  type extractop = (extract, extract, extract, extract, extract, extract, extract) v128op
   type shift = Shl | ShrS | ShrU
   type shiftop = (shift, shift, shift, shift, shift, shift, shift) v128op
 end
